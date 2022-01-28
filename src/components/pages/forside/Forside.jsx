bd1cf846ff5a75f5248373a2ec130ffbc12b1330
Eva-Renata <all2evi@yahoo.com>
2022-01-28 08:28:04 +0100

    nyhedsbrev

--- src/components/pages/forside/Forside.jsx
+++ src/components/pages/forside/Forside.jsx
@@ -1,12 +1,14 @@
 import style from './Forside.module.scss'
 import {Slider}  from '../../partials/slider/Slider'
 import { News } from '../../partials/news/News'
+import { Nyhedsbrev } from '../../partials/nyhedsbrev/Nyhedsbrev'
 
 export const Forside = () => {
     return(
      <section>
        <Slider />
        <News />
+       <Nyhedsbrev />
      </section>
     )
 }
---
+++ src/components/partials/nyhedsbrev/Nyhedsbrev.jsx
@@ -0,0 +1,20 @@
+import styles from './Nyhedsbrev.module.scss'
+import background from '../../../assets/Images/newsletterbg.jpg'
+
+export const Nyhedsbrev = () => {
+    return (
+        <div className={styles.wrapper} style={{ backgroundImage: `url(${background})`, width:'100%'}}>
+            <section className={styles.nyhedsbrev} >
+                <h3>Tilmeld dig vores nyhedsbrev</h3>
+                <p>Der er mange tilgængelige  udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået forandringer</p>
+
+                <form >
+                    <input type="email" placeholder="Indtast din email....." />
+                    <button type="submit">TILMELD</button>
+                </form>
+            </section>
+        </div>
+        
+        
+    )
+}
